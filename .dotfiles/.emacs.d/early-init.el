2025f665ab9bbd5b9248d90242dc560271890586
Alex Rosegrid <rosegrid58@gmail.com>
2024-11-14 22:10:03 +0530

    .

---
+++ .dotfiles/.emacs.d/early-init.el
@@ -0,0 +1,13 @@
+(setq default-frame-alist '(
+  (background-color . "#000000")))
+
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+
+(setq display-line-numbers 'relative)
+
+(setq-default truncate-lines t)
+(setq-default blink-cursor-mode nil)
+
+(setq make-backup-files nil)
