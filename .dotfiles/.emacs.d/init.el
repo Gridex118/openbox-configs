3b8aae1cc321446ad13d38c0ce453ad9472b1055
Alex Rosegrid <rosegrid58@gmail.com>
2024-11-19 13:49:15 +0530

    .

--- .dotfiles/.emacs.d/init.el
+++ .dotfiles/.emacs.d/init.el
@@ -31,10 +31,11 @@
    '(:foreground default :background default :scale 2 :html-foreground "Black" :html-background "Transparent" :html-scale 1.0 :matchers
 		 ("begin" "$1" "$" "$$" "\\(" "\\[")))
  '(package-selected-packages
-   '(yasnippet-snippets yasnippet all-the-icons auctex corfu undo-fu vertico marginalia doom-themes doom-modeline lsp-mode)))
+   '(company highlight-defined org-bullets swiper yasnippet-snippets yasnippet all-the-icons auctex corfu undo-fu vertico marginalia doom-themes doom-modeline lsp-mode)))
 
 ;; UI, and stuff
 (require 'all-the-icons) ; Because, for some reason, these aren't auto loaded
+(company-mode 1)
 (marginalia-mode)
 (vertico-mode)
 
@@ -47,30 +48,42 @@
 (setq column-number-mode t)
 (put 'erase-buffer 'disabled nil)
 
-;; Org Mode Settings
-(setq org-latex-create-formula-image-program 'dvipng)
-
-;; Programming options
-(setq major-mode-remap-alist
-      '((c-mode . c-ts-mode)
-	(c++-mode . c++-ts-mode)))
-(setq c-default-style "stroustrup")
-(setq lsp-enable-on-type-formatting nil)
-(setq treesit-font-lock-level 4)
-(put 'set-goal-column 'disabled nil)
-(add-hook 'prog-mode-hook
-	  (lambda()
-	    (lsp)
-	    (yas-global-mode 1)
-	    (yas-reload-all)))
-(add-hook 'c-mode-hook
-          (lambda()
-	    (corfu-mode 1)))
-(add-hook 'c++-mode-hook
-	  (lambda()
-	    (corfu-mode 1)))
-
 ;; Tree Sitter
 (setq treesit-language-source-alist
       '((cpp "https://github.com/tree-sitter/tree-sitter-cpp" "v0.23.4")
 	(c "https://github.com/tree-sitter/tree-sitter-c" "v0.22.7")))
+
+;; Elisp Mode Settings
+(add-hook 'emacs-lisp-mode-hook
+	  (lambda()
+	    (highlight-defined-mode 1)))
+
+;; Programming options
+(setq c-default-style "k&r")
+(setq c-basic-offset 4)
+(setq lsp-enable-on-type-formatting nil)
+(setq treesit-font-lock-level 4)
+(setq major-mode-remap-alist
+      '((c-mode . c-ts-mode)
+	(c++-mode . c++-ts-mode)))
+(put 'set-goal-column 'disabled nil)
+;; Honestly, I don't really know if the previous style setting even matters
+(setq c-ts-mode-indent-style 'k&r
+      c-ts-mode-indent-offset 4)
+(add-hook 'prog-mode-hook
+	  (lambda()
+	    (yas-global-mode 1)
+	    (yas-reload-all)
+	    (corfu-mode 1)))
+(add-hook 'c-ts-mode-hook
+          (lambda()
+	    (lsp)))
+(add-hook 'c++-ts-mode-hook
+	  (lambda()
+	    (lsp)))
+
+;; Org Mode Settings
+(setq org-latex-create-formula-image-program 'dvipng)
+(add-hook 'org-mode-hook
+	  (lambda()
+	    (org-bullets-mode 1)))
